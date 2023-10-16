7e55839bdace5dd7a966f5d1e5a8c991e372da21
Leela-Sumanth-Dharmana <leelasumanth999@gmail.com>
2023-10-16 16:23:16 +0530

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,10 @@
+function Contact(){
+    return(
+        <div>
+            <div id='contactdet' className='container-fluid text-start'>Mobile Number:<span>9110793549</span></div>
+            <div id='contactdet' className='container-fluid text-start'>Email ID:<span>leelasumanth999@gmail.com</span></div>
+            <div id='contactdet' className='container-fluid text-start'>Address: <span>Naduma Street, Mabagam, Srikakulam district, Andhra Pradesh, India 532421</span></div>
+        </div>
+    )
+}
+export default Contact;
